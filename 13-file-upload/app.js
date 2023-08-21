ca1d4015a301adb73a490fbb6d5a0c4c2377f6cf
seokmin <msm0748@gmail.com>
2023-08-21 17:24:53 +0900

    study: 동적 파일 업로드

--- 13-file-upload/app.js
+++ 13-file-upload/app.js
@@ -4,11 +4,10 @@ const PORT = 8000;
 
 // multer 관련 설정
 const multer = require('multer');
-const path = require('path');
+const path = require('path'); // 경로에 관한 내장 모듈
 const upload = multer({
   dest: 'uploads/', // dest: 클라이언트가 업로드한 파일을 저장할 서버측 경로
 });
-
 // multer 세부 설정
 const uploadDetail = multer({
   // storage: 저장할 공간에 대한 정보
@@ -30,6 +29,7 @@ const uploadDetail = multer({
 
 app.set('view engine', 'ejs');
 app.use('/views', express.static(__dirname + '/views'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -41,33 +41,34 @@ app.get('/', (req, res) => {
 // upload.single('userfile'): 클라이언트의 요청이 들어오면
 // multer 설정(upload 변수)에 따라 파일을 업로드 한 후, req.file 객체 생성
 // single() 인자는 input 태그의 name 속성과 일치시켜야 함
+// app.post('/upload', upload.single('userfile'), (req, res) => {
 app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
   // req.file: 파일 업로드 정보
   // req.body: 파일 외의 정보들
-  console.log(req.file); // {파일1_정보}
+  console.log(req.file); // { 파일_정보 }
   console.log(req.body);
   res.send('파일 업로드 완료!');
 
   // req.file 객체 자세히 보기
   // {
   //   fieldname: 'userfile', // 폼에 정의한 name 값
-  //   originalname: 'á\x84\x8Cá\x85¦á\x84\x86á\x85©á\x86.png', // 원본 파일명
+  //   originalname: 'apple1.png', // 원본 파일명
   //   encoding: '7bit', // 파일 인코딩 타입
   //   mimetype: 'image/png', // 파일 타입
   //   destination: 'uploads/', // 파일 저장 경로
-  //   filename: '4b43e5750ef0038c6f5abe6f597cd500', // destination 저장된 파일명
-  //   path: 'uploads/4b43e5750ef0038c6f5abe6f597cd500', // 업로드된 파일 전체 경로
-  //   size: 782274 // 파일 크기
+  //   filename: '88b790f556b9873a7bce570061d8ceb8',  // destination 저장된 파일명
+  //   path: 'uploads\\88b790f556b9873a7bce570061d8ceb8', // 업로드된 파일 전체 경로
+  //   size: 2515 // 파일 크기
   // }
 });
 
 // 2. array(): 여러 파일을 한 번에 업로드
-// uploadDetail.array('userfiles): 클라이언트 요청이 들어오면
+// uploadDetail.array('userfiles'): 클라이언트 요청이 들어오면
 // multer 설정(uploadDetail 변수)에 따라 파일을 업로드한 후, req.files 객체 생성
 app.post('/upload/array', uploadDetail.array('userfiles'), (req, res) => {
-  console.log(req.files); // [{파일1_정보}, {파일2_정보}, ..] : 배열 형태로 각 파일 정보를 출력
+  console.log(req.files); // [ { 파일1_정보 }, { 파일2_정보 }, .. ] : 배열 형태로 각 파일 정보를 출력
   console.log(req.body);
-  res.send('하나의 인풋에 여러 파일 업로드 완료!!');
+  res.send('하나의 인풋에 여러 파일 업로드 완료!');
 });
 
 // 3. fields(): 여러 파일을 각각 인풋에 업로드
@@ -77,6 +78,12 @@ app.post('/upload/fields', uploadDetail.fields([{ name: 'userfile1' }, { name: '
   console.log(req.files); // { userfile1: [ {파일_정보} ], userfile2: [ {파일_정보} ]} 객체 안에 배열 형태로 각 파일 정보
   console.log(req.body);
   res.send('하나의 인풋에 여러 파일 업로드 완료!');
+});
+
+// 동적 폼 전송
+app.post('/dynamicFile', uploadDetail.single('dynamicUserfile'), (req, res) => {
+  console.log(req.file);
+  res.send(req.file);
 });
 
 app.listen(PORT, function () {
