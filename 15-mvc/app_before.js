a9fdbb87da815481960e6561c9bf2a315e97d08d
seokmin <msm0748@gmail.com>
2023-08-25 17:51:38 +0900

    study: mvc 적용 전 코드

---
+++ 15-mvc/app_before.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// (임시) DB로부터 받아온 댓글 목록
+const comments = [
+  {
+    id: 1,
+    userid: 'helloworld',
+    date: '2022-10-31',
+    comment: '안녕하세요^~^',
+  },
+  {
+    id: 2,
+    userid: 'happy',
+    date: '2022-11-01',
+    comment: '반가워유',
+  },
+  {
+    id: 3,
+    userid: 'lucky',
+    date: '2022-11-02',
+    comment: '오 신기하군',
+  },
+  {
+    id: 4,
+    userid: 'bestpart',
+    date: '2022-11-02',
+    comment: '첫 댓글입니당ㅎㅎ',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/comments', (req, res) => {
+  res.render('comments', {
+    comments,
+  });
+});
+
+app.get('/comment/:id', (req, res) => {
+  // req.params: 라우트 매개변수에 대한 정보가 담겨 있음
+  console.log(req.params); // { id: '1' }
+  const cmtId = Number(req.params.id); // 댓글의 고유 아이디
+
+  if (!comments[cmtId - 1]) {
+    return res.render('404');
+  }
+
+  res.render('comment', { comment: comments[cmtId - 1] });
+});
+
+app.get('/test/:id/:name', (req, res) => {
+  console.log(req.params);
+  res.send('test res success!');
+});
+
+// 404 error 처리
+// 반드시 맨 마지막 라우트로 선언
+app.get('*', (req, res) => {
+  res.render('404');
+});
+
+app.listen(PORT, () => {
+  console.log(`Port ${PORT} is opening`);
+});
