ec2c163ff0c546a43d74e1d84c72b345688c1a1e
seokmin <msm0748@gmail.com>
2023-08-28 18:24:13 +0900

    study: mvc with mysql로 delete 구현

--- 15-mvc_mysql/controller/Cvisitor.js
+++ 15-mvc_mysql/controller/Cvisitor.js
@@ -9,18 +9,29 @@ exports.getVisitors = (req, res) => {
   // res.render('visitor', { data: Visitor.getVisitors() });
 
   // [after]
-  // console.log(Visitor.getVisitors());
+  // console.log(Visitor.getVisitors())
   Visitor.getVisitors((result) => {
-    console.log('controller >> ', result);
+    console.log('controller >>', result);
     res.render('visitor', { data: result });
   });
 };
 
 exports.postVisitor = (req, res) => {
-  console.log('controller >> ', req.body);
-  // res.send({ result: true });
+  console.log(req.body); // { name: xx, comment: yy }
+  const { name, comment } = req.body;
+
   Visitor.postVisitor(req.body, (insertId) => {
     console.log('controller >> ', insertId);
-    res.send({ id: insertId, name: req.body.name, comment: req.body.comment });
+    res.send({ id: insertId, name: name, comment: comment });
+  });
+};
+
+exports.deleteVisitor = (req, res) => {
+  console.log(req.body); // { id: xx }
+  const { id } = req.body;
+
+  Visitor.deleteVisitor(id, (result) => {
+    console.log('controller >>', result); // true
+    res.send(result); // res.send(true)
   });
 };
--- 15-mvc_mysql/model/Visitor.js
+++ 15-mvc_mysql/model/Visitor.js
@@ -1,10 +1,3 @@
-// exports.getVisitors = () => {
-//   return [
-//     { id: 1, name: '홍길동', comment: '내가 왔다.' },
-//     { id: 2, name: '이찬혁', comment: '으라차차' },
-//   ];
-// };
-
 const mysql = require('mysql');
 
 // db 연결 설정
@@ -15,7 +8,7 @@ const conn = mysql.createConnection({
   database: 'sesac',
 });
 
-exports.getVisitors = (cb) => {
+exports.getVisitors = (callback) => {
   // [before]
   //   return [
   //     { id: 1, name: '홍길동', comment: '내가 왔다.' },
@@ -23,27 +16,39 @@ exports.getVisitors = (cb) => {
   //   ];
 
   // [after]
-  conn.query('SELECT * FROM visitor', (err, rows) => {
+  conn.query('select * from visitor', (err, rows) => {
     if (err) {
       throw err;
     }
-
     console.log('model >>', rows);
-    cb(rows);
+    callback(rows);
   });
 };
 
-exports.postVisitor = (data, cb) => {
+exports.postVisitor = (data, callback) => {
   // 매개변수
-  // data: 프론트엔드에서 유저가 입력한 값(req.body)
+  // data: 프론트엔드에서 유저가 입력한 값 (req.body)
   // callback: query 실행 후 호출할 함수
-  console.log('와다다다다다닥');
-
-  conn.query(`insert into visitor values(null, "${data.name}", "${data.comment}")`, (err, rows) => {
+  const { name, comment } = data;
+  conn.query(`insert into visitor values(null, "${name}", "${comment}")`, (err, rows) => {
     if (err) {
       throw err;
     }
+
     console.log('model >>', rows);
-    cb(rows.insertId);
+    callback(rows.insertId);
+  });
+};
+
+exports.deleteVisitor = (id, callback) => {
+  console.log('model >>', id); // front에서 알려준 삭제할 데이터의 pk
+
+  conn.query(`delete from visitor where id=${id}`, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('model >> ', rows);
+    callback(true); // { id: id }로 쓸 수도 있음
   });
 };
