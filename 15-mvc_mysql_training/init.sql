aea211a8f2a0b81251002cfb49b0ed736b7a62bb
seokmin <msm0748@gmail.com>
2023-08-29 16:46:50 +0900

    study: mvc-mysql 데이터베이스 세팅

--- 15-mvc_mysql_training/init.sql
+++ 15-mvc_mysql_training/init.sql
@@ -11,6 +11,12 @@ SHOW tables;
 DROP TABLE IF EXISTS user;
 
 -- TODO: user 데이터베이스 생성
+CREATE TABLE user (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    userid VARCHAR(20) NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    pw VARCHAR(20) NOT NULL
+);
 
 -- user 데이블 데이터 추가
 INSERT INTO user (userid, name, pw) VALUES ('sean', 'sean', '1234');
