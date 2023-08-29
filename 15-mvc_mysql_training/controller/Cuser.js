a66f5d43080722f224c1fc33c8cf291d6ede953e
seokmin <msm0748@gmail.com>
2023-08-29 16:44:49 +0900

    study: mvc-mysql 경로 설정

--- 15-mvc_mysql_training/controller/Cuser.js
+++ 15-mvc_mysql_training/controller/Cuser.js
@@ -1,1 +1,16 @@
 // TODO: 컨트롤러 코드
+
+// main view
+exports.main = (req, res) => {
+  res.render('index');
+};
+
+// signup view
+exports.getSignup = (req, res) => {
+  res.render('signup');
+};
+
+// signin view
+exports.getSignin = (req, res) => {
+  res.render('signin');
+};
--- 15-mvc_mysql_training/routes/user.js
+++ 15-mvc_mysql_training/routes/user.js
@@ -1,1 +1,10 @@
 // TODO: 라우트 설정
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/Cuser');
+
+router.get('/', controller.main);
+router.get('/signup', controller.getSignup);
+router.get('/signin', controller.getSignin);
+
+module.exports = router;
