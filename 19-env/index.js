ce26c092ada9e0a7366c8369ff122354a4fe07c4
seokmin <msm0748@gmail.com>
2023-09-04 16:14:33 +0900

    study: cross-env 환경변수 관리

--- 19-env/index.js
+++ 19-env/index.js
@@ -10,7 +10,7 @@ const PORT = 8000;
 
 app.get('/', (req, res) => {
   console.log(process.env.NAME);
-  console.log(process.env.NODE);
+  console.log(process.env.NODE_ENV);
   res.send('Hello, world!');
 });
 
