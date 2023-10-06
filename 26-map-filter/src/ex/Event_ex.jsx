45826fae8e7764756917385739edff17bfdcff27
seokmin <msm0748@gmail.com>
2023-10-06 14:05:22 +0900

    study: react state 실습

--- 26-map-filter/src/ex/Event_ex.jsx
+++ 26-map-filter/src/ex/Event_ex.jsx
@@ -1,77 +1,153 @@
-import { useRef } from 'react';
 import { useState } from 'react';
 
-function Event_ex() {
-  const nameRef = useRef();
-  const titleRef = useRef();
-  const [name, setName] = useState('');
-  const [title, setTitle] = useState('');
-  const [select, setSelect] = useState('작성자');
-  const [search, setSearch] = useState('');
-  const [data, setData] = useState([]);
+const Ex = () => {
+  const [inputs, setInputs] = useState({
+    writer: '',
+    title: '',
+    search: '',
+  }); // 하나의 state로 input 여러개 관리하기
+  const { writer, title, search } = inputs; // 객체 구조분해
+  const [comment, setComment] = useState([
+    {
+      writer: '민수',
+      title: '안뇽',
+    },
+    {
+      writer: '지민',
+      title: '하이하이',
+    },
+    {
+      writer: '희수',
+      title: '멋쟁이',
+    },
+  ]);
+  const [result, setResult] = useState([]);
+  const [searchType, setSearchType] = useState('writer');
 
-  const handleSubmit = () => {
-    if (name.trim() === '') return nameRef.current.focus();
-    if (title.trim() === '') return titleRef.current.focus();
-    setData((prev) => [...prev, { id: data.length + 1, title, name }]);
-    setName('');
-    setTitle('');
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
 
-  const handleSearch = () => {
-    // if (select === '작성자') {
-    //   const newData = data.filter((v) => v.name === select);
-    // }
+  const addComment = () => {
+    const newComment = {
+      writer,
+      title,
+    };
+
+    setComment([...comment, newComment]);
+    setInputs({
+      ...inputs,
+      writer: '',
+      title: '',
+    }); // input 초기화
   };
+
+  const searchComment = () => {
+    let searchResult = comment.filter((item) => {
+      // console.log(item); // comment에 대한 각 원소(객체)
+      // console.log(item[searchType]); // 검색 조건(key)에 대한 value 값
+      // console.log(item[searchType].includes(search)); // true or false
+
+      if (!item[searchType].includes(search)) {
+        return null; // 검색결과 없음; null 반환
+      }
+
+      return item; // 검색결과 있음; 검색결과(배열) 반환
+    });
+
+    setResult(searchResult); // 검색 결과 state 설정
+    setInputs({
+      ...inputs,
+      search: '',
+    });
+  };
+
+  const selectSearchType = (e) => {
+    setSearchType(e.target.value); // select option 상태 설정
+  };
+
   return (
-    <div>
-      <div style={{ border: '1px solid' }}>
-        <label htmlFor="name">작성자 : </label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="작성자"
-          ref={nameRef}
-        />
-        <label htmlFor="title">제목 : </label>
-        <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} ref={titleRef} />
-        <button type="button" onClick={handleSubmit}>
+    <div style={{ margin: '200px 0' }}>
+      <form>
+        <label htmlFor="wirter2">작성자:</label>
+        <input id="wirter2" type="text" name="writer" value={writer} onChange={onChange} />
+        <label htmlFor="title2">제목:</label>
+        <input id="title2" type="text" name="title" value={title} onChange={onChange} />
+        <button type="button" onClick={addComment}>
           작성
         </button>
-      </div>
-      <div>
-        <select onChange={(e) => setSelect(e.target.value)}>
-          <option value="작성자">작성자</option>
-          <option value="제목">제목</option>
+      </form>
+
+      <form>
+        <select name="type" value={searchType} onChange={selectSearchType}>
+          <option value="writer">작성자</option>
+          <option value="title">제목</option>
         </select>
-        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-        <button type="button" onClick={handleSearch}>
+
+        <input type="text" name="search" placeholder="검색어" value={search} onChange={onChange} />
+        <button type="button" onClick={searchComment}>
           검색
         </button>
-      </div>
-      <div>
-        <table border="1" cellSpacing="0" cellPadding="10">
-          <thead>
-            <tr>
-              <th>번호</th>
-              <th>제목</th>
-              <th>작성자</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((v) => (
-              <tr key={v.id}>
-                <td>{v.id}</td>
-                <td>{v.title}</td>
-                <td>{v.name}</td>
+      </form>
+
+      <h3>전체 댓글 목록</h3>
+      <table border={1} style={{ margin: '30px auto', width: '500px' }}>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {comment.map((value, idx) => {
+            // value = { writer: xxx , titlex: xxx };
+            console.log(idx);
+            return (
+              <tr key={idx + 1}>
+                <td>{idx + 1}</td>
+                <td>{value.title}</td>
+                <td>{value.writer}</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <h3>댓글 검색 결과</h3>
+      {result.length > 0 ? (
+        <div>
+          <table border={1} style={{ margin: '30px auto', width: '500px' }}>
+            <thead>
+              <tr>
+                <th>번호</th>
+                <th>제목</th>
+                <th>작성자</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.map((value, idx) => {
+                // value = { writer: xxx , title: xxx };
+                return (
+                  <tr key={idx + 1}>
+                    <td>{idx + 1}</td>
+                    <td>{value.title}</td>
+                    <td>{value.writer}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h5>검색 결과가 없습니다.</h5>
+      )}
     </div>
   );
-}
-export default Event_ex;
+};
+
+export default Ex;
