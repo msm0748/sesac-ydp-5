d39b01d6f57a0cb3245a1c2ca1dea17d1258e965
seokmin <msm0748@gmail.com>
2023-10-11 14:18:50 +0900

    study: css module

--- 17-sequelize/models/index.js
+++ 17-sequelize/models/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// sequelize 모듈 호출
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // config.json 파일을 불러와서 development 환경의 db 설정
 // config : db 접근 가능한 설정 값 저장
@@ -23,7 +22,7 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 // !! models/ 폴더에 정의되는 model(테이블)은 db 객체에 저장
-db.Visitor = require('./Visitor')(sequelize, Sequelize);
+db.Visitor = require('./Visitor')(sequelize, DataTypes);
 
 // db 객체 내보냄 (모듈화 내보냄. 다른 곳에서 db 객체 사용 가능)
 module.exports = db;
---
+++ 29-styles/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import CssModuleComponents from './CssModuleComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>안뇽</h1>
+      <CssModuleComponents />
+    </div>
+  );
+}
+
+export default App;
