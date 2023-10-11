13ab3ee2cc8f4a0f572b4a24b1f283480ff534db
seokmin <msm0748@gmail.com>
2023-10-11 15:35:29 +0900

    study: sass React

--- 29-styles/src/App.jsx
+++ 29-styles/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import CssModuleComponents from './CssModuleComponents';
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>안뇽</h1>
       <CssModuleComponents />
+      <SassComponent />
+      <StyledComponent />
     </div>
   );
 }
